a30ae9560b67b7bca1026a969d6fcb519768dfae
ismaila sylla <ismailasyllaa@gmail.com>
2021-01-27 16:24:28 +0400

    Form validation

--- src/app/user-settigns-form/user-settigns-form.component.ts
+++ src/app/user-settigns-form/user-settigns-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -8,19 +9,33 @@ import { UserSettings } from '../data/user-settings';
 })
 export class UserSettignsFormComponent implements OnInit {
 
+// originalUserSettings :UserSettings = {
+//   name: "ismail",
+//   password : "American1",
+//   emailOffers:true,
+//   interfaceStyle:'dark',
+//   subscriptionType:'Annualy',
+//   notes: "Just some notes..."
+// }
 originalUserSettings :UserSettings = {
-  name: "ismail",
-  password : "American1",
-  emailOffers:true,
-  interfaceStyle:'dark',
-  subscriptionType:'Annualy',
-  notes: "Just some notes..."
+  name: null,
+  password : null,
+  emailOffers:null,
+  interfaceStyle:null,
+  subscriptionType:null,
+  notes: null,
 }
+
+
 
 userSettings :UserSettings = {... this.originalUserSettings}
   constructor() { }
 
   ngOnInit(): void {
   }
+
+onSubmit(form: NgForm){
+  console.log("Submitted", form.valid)
+}
 
 }
