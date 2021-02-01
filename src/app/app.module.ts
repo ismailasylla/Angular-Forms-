59005107fc6f850857fff77c05bdeb5a2836f8bd
ismaila sylla <ismailasyllaa@gmail.com>
2021-02-01 14:51:53 +0400

    Added 3rd party form controls ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserSettignsFormComponent } from './user-settigns-form/user-settigns-form.component';
+import {ButtonsModule} from 'ngx-bootstrap/buttons'
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { UserSettignsFormComponent } from './user-settigns-form/user-settigns-fo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
