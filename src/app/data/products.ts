f1d0704336eb19b4f6979797716f52c493d8e381
ismaila sylla <ismailasyllaa@gmail.com>
2021-02-08 12:25:16 +0400

    added rating

---
+++ src/app/data/products.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+    product_id:string 
+    name: string
+    brand: string
+    description: string
+    price: number
+    rating: number
+    numReviews:number
+    countInStock:number
+    image:string
+    category:string
+    
+    
+}
